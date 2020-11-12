77c3680fd8743462f77ecedc95772f0759beca84
Ghoulboy78 <ghoulboy27@gmail.com>
2020-11-12 09:44:37 +0100

    fixes #537

--- src/main/java/carpet/script/value/EntityValue.java
+++ src/main/java/carpet/script/value/EntityValue.java
@@ -476,6 +476,11 @@ public class EntityValue extends Value
             return Value.NULL;
         });
 
+        put("absorption",(e, a)->{
+            if(e instanceof PlayerEntity) return new NumericValue(((PlayerEntity) e).getAbsorptionAmount());
+            return Value.NULL;
+        });
+
         put("jumping", (e, a) -> {
             if (e instanceof LivingEntity)
             {
@@ -1292,6 +1297,10 @@ public class EntityValue extends Value
 
         put("add_exhaustion", (e, v)-> {
             if(e instanceof PlayerEntity) ((PlayerEntity) e).getHungerManager().addExhaustion((int) NumericValue.asNumber(v).getLong());
+        });
+
+        put("absorption",(e, v)->{
+            if(e instanceof PlayerEntity) ((PlayerEntity) e).setAbsorptionAmount((float) NumericValue.asNumber(v).getLong());
         });
 
         put("saturation", (e, v)-> {
