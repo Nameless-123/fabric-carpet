eeac81ec10b303276c8046e5f6902a534662b504
Ghoulboy <ghoulboy27@gmail.com>
2022-08-05 15:20:00 +0200

    Adding docs

--- src/main/java/carpet/mixins/MobMixin_scarpetEventsMixin.java
+++ src/main/java/carpet/mixins/MobMixin_scarpetEventsMixin.java
@@ -10,13 +10,23 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+/**
+ * This calls on_transform for the following conversions:
+ * <li>Husk -> Zombie</li>
+ * <li>Zombie -> Drowned</li>
+ * <li>Villager -> Zombie Villager</li>
+ * <li>Zombie Villager -> Villager</li>
+ * <li>Piglin / Piglin brute -> Pigman</li>
+ * <li>Hoglin -> Zoglin</li>
+ * <li>Skeleton -> Stray</li>
+ */
 @Mixin(Mob.class)
 public class MobMixin_scarpetEventsMixin {
     @Inject(
         method = "convertTo",
         at = @At("RETURN")
     )
-    public <T extends  Mob> void mixin(EntityType<T> entityType, boolean bl, CallbackInfoReturnable<T> cir){
+    public <T extends  Mob> void onConvertEvent(EntityType<T> entityType, boolean bl, CallbackInfoReturnable<T> cir){
         ((EntityInterface)this).getEventContainer().onEvent(EntityEventsGroup.Event.ON_TRANSFORM, cir.getReturnValue());
     }
 }
