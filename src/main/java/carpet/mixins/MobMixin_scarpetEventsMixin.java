6b3104adf22a39315713b7097f846151a934a409
Ghoulboy <ghoulboy27@gmail.com>
2022-08-06 10:25:17 +0200

    removing unused import

--- src/main/java/carpet/mixins/MobMixin_scarpetEventsMixin.java
+++ src/main/java/carpet/mixins/MobMixin_scarpetEventsMixin.java
@@ -2,7 +2,6 @@ package carpet.mixins;
 
 import carpet.fakes.EntityInterface;
 import carpet.script.EntityEventsGroup;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import org.spongepowered.asm.mixin.Mixin;
